58a0bc8ce1fde6b2f8a0cf03ed7a0e5173d49965
Md. Jabed Ahmed <jabedahmedrifat1@gmail.com>
2023-09-17 17:30:57 +0600

    model added

--- user/models.py
+++ user/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    is_developer = models.BooleanField('student', default=False)
+    is_marketer = models.BooleanField('marketer', default=False)
+    is_tester = models.BooleanField('tester', default=False)
+    
+    def __str__(self):
+        return self.is_developer
+    
+    def __str__(self):
+        return self.is_marketer
+    
+    def __str__(self):
+        return self.is_tester
+    
+    
+    
+    
+    
+
